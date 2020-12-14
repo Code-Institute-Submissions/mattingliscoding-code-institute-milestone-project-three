7e4d71800260a3d2bb3f8a260a65902761c999dd
mattingliscoding <mattingliscoding@gmail.com>
2020-12-14 06:28:58 +0000

    Quest accordion changed from materialize for bootstrap

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,6 @@
-// SIDEBAR
 
 // Hide submenus
 $('#body-row .collapse').collapse('hide'); 
 
-// Collapse/Expand icon
+// Profile Quests Collapse/Expand icon
 $('.collapse-icon').addClass('fa fa-caret-down'); 
-
-
-
