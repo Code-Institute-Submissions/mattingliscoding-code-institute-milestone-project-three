9239e2f38e6fb9cccded29c380297f2dd3cb39a0
mattingliscoding <mattingliscoding@gmail.com>
2020-12-16 03:56:35 +0000

    Delete user account bug fix

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def register():
         mongo.db.users.insert_one(register)
 
         # put the new user into 'session' cookie
-        session["user"] = request.form.get("username")()
+        session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
         return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
